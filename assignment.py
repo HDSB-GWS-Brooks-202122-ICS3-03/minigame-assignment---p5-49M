7adcfeaa7499ff7a55b0250a0c0e4759e7a73a22
49M <1buczekmic@hdsb.ca>
2022-04-06 18:56:54 -0400

    Movement has been added through a boolean feature which makes direction changes smooth and quick. Movement is controlled by arrow keys, moving when held down and stopping when released. Horizontal and vertical boundaries have been established and work for the initial collision.

--- assignment.py
+++ assignment.py
@@ -9,7 +9,8 @@
 # I think this project deserves a level XXXXXX because ...
 #
 # Features Added:
-#   ...
+#   Movement with boolean functions to make transitions from keys smoother. When two keys are pressed by accident at
+#   the same time, movement does not stop and goes in the most recently clicked key direction.
 #   ...
 #   ...
 # ---------------------------------------------------------------------------------------#
@@ -29,11 +30,14 @@ def main():
     mainSurface = pygame.display.set_mode((surfaceSize, surfaceSize - 100))
 
     # -----------------------------Program Variable Initialization-----------------------#
-    # Set up some data to describe a small circle and its color
     rocketBaseColor = (171, 186, 185)  # A color is a mix of (Red, Green, Blue)
     rocketWingColor = (156, 5, 5)
-    rocketX = 435
-    rocketY = 250
+    rocketPos = [435, 250]
+    rocketSpeed = [0, 0]
+    moveLeft = False
+    moveRight = False
+    moveUp = False
+    moveDown = False
 
     # -----------------------------Main Game Loop----------------------------------------#
     while True:
@@ -42,46 +46,96 @@ def main():
         ev = pygame.event.poll()  # Look for any event
         if ev.type == pygame.QUIT:  # Window close button clicked?
             break  # ... leave game loop
+        #  Enabling movement with arrow keys when pressed down
         elif ev.type == pygame.KEYDOWN:  # KEYDOWN has the attributes: key, mod, unicode, scancode
             print('A Key was pressed down.  ', end='')
             print(f'key: {ev.key}, mod: {ev.mod}, unicode: {ev.unicode}, scancode: {ev.scancode}')
-
+            #  movement in desired direction
             if ev.key == pygame.K_LEFT:
                 print('Move Left')
-                rocketX -= 10
+                moveLeft = True
+                if moveLeft:
+                    rocketSpeed[0] = -10
+
             elif ev.key == pygame.K_RIGHT:
                 print('Move Right')
-                rocketX += 10
+                moveRight = True
+                if moveRight:
+                    rocketSpeed[0] = 10
+
+            elif ev.key == pygame.K_UP:
+                print('Move Up')
+                moveUp = True
+                if moveUp:
+                    rocketSpeed[1] = -10
+
+            elif ev.key == pygame.K_DOWN:
+                print('Move Down')
+                moveDown = True
+                if moveDown:
+                    rocketSpeed[1] = 10
+
+        #  Stops movement when arrow key is released
+        #  Boolean usage to make transitions from keys more smooth and not stopping rocket when two clicked at the same
+        #  time.
+        elif ev.type == pygame.KEYUP:
+            if ev.key == pygame.K_LEFT:
+                moveLeft = False
+                if not moveLeft and not moveRight:
+                    rocketSpeed[0] = 0
+
+            elif ev.key == pygame.K_RIGHT:
+                moveRight = False
+                if not moveRight and not moveLeft:
+                    rocketSpeed[0] = 0
+
+            elif ev.key == pygame.K_UP:
+                moveUp = False
+                if not moveUp and not moveDown:
+                    rocketSpeed[1] = 0
+
+            elif ev.key == pygame.K_DOWN:
+                moveDown = False
+                if not moveDown and not moveUp:
+                    rocketSpeed[1] = 0
 
         # -----------------------------Game Logic----------------------------------------#
-        # Update your game objects and data structures here...
+        # Rocket movement based on the rocket speed
+        rocketPos[0] += rocketSpeed[0]
+        rocketPos[1] += rocketSpeed[1]
+
+        #  Horizontal boundary for rocket ship
+        if rocketPos[0] <= 5 or rocketPos[0] >= 865:
+            rocketSpeed[0] = 0
+
+        #  Vertical boundary for rocket ship
+        if rocketPos[1] <= 0 or rocketPos[1] >= 695:
+            rocketSpeed[1] = 0
 
         # -----------------------------Drawing Everything--------------------------------#
-        # We draw everything from scratch on each frame.
-        # So first fill everything with the background color
+
         mainSurface.fill((39, 1, 59))
 
-        # Draw a circle on the surface
-        pygame.draw.ellipse(mainSurface, rocketBaseColor, (rocketX, rocketY, 30, 90))
-        pygame.draw.polygon(mainSurface, (104, 115, 114), [(rocketX + 3, rocketY + 70), (rocketX + 27, rocketY + 70),
-                                                           (rocketX - 8, rocketY + 105), (rocketX + 38, rocketY + 105)
-                                                           ])
-        pygame.draw.polygon(mainSurface, rocketWingColor, [(rocketX + 2, rocketY + 20), (rocketX + 2, rocketY + 69),
-                                                           (rocketX - 12, rocketY + 60)])
-        pygame.draw.polygon(mainSurface, rocketWingColor, [(rocketX + 28, rocketY + 20), (rocketX + 28, rocketY + 69),
-                                                           (rocketX + 42, rocketY + 60)])
+        #  Rocket design (All parts are connected throughout movement)
+        pygame.draw.ellipse(mainSurface, rocketBaseColor, (rocketPos[0], rocketPos[1], 30, 90))  # rockets main body
+        pygame.draw.polygon(mainSurface, (104, 115, 114), [(rocketPos[0] + 3, rocketPos[1] + 70), (rocketPos[0] + 27,
+                                                                                                   rocketPos[1] + 70),
+                                                           (rocketPos[0] - 8, rocketPos[1] + 105), (rocketPos[0] + 38,
+                                                                                                    rocketPos[1] + 105)
+                                                           ])  # Bottom of Rocket
+        pygame.draw.polygon(mainSurface, rocketWingColor, [(rocketPos[0] + 2, rocketPos[1] + 20), (rocketPos[0] + 2,
+                                                                                                   rocketPos[1] + 69),
+                                                           (rocketPos[0] - 12, rocketPos[1] + 60)])  # Left wing
+        pygame.draw.polygon(mainSurface, rocketWingColor, [(rocketPos[0] + 28, rocketPos[1] + 20), (rocketPos[0] + 28,
+                                                                                                    rocketPos[1] + 69),
+                                                           (rocketPos[0] + 42, rocketPos[1] + 60)])  # Right wing
 
-    # -----------------------------Main Game Loop----------------------------------------#
-    while True:
-
-        # -----------------------------Event Handling---------------
-
-        # Now the surface is ready, tell pygame to display it!
+        # Surface display
         pygame.display.flip()
 
-        clock.tick(60)  # Force frame rate to be slower
+        clock.tick(60)  # Forces frame rate to be slower
 
-    pygame.quit()  # Once we leave the loop, close the window.
+    pygame.quit()  # Once loop is left, closes the window.
 
 
 main()
